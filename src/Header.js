177d5eb1ec6e8900a5f27dd01a38469281dc1790
apg868 <123202954+apg868@users.noreply.github.com>
2023-08-09 17:04:40 -0400

    Added temp css stylesheet for Header.js component. To be done for rest of components once UI prototype is finalized

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,12 @@
+import React from 'react';
+import './Header.css';
 
-
-
-function Header(){
-
-
-
-    return(
-
-        <h1> h1 header</h1>
-
-    )
+function Header() {
+  return (
+    <div className="gridBox">
+      <h1 className="h1-header">h1 header</h1>
+    </div>
+  );
 }
 
 export default Header;
