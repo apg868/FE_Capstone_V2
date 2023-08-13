5dff472432f75862ab680f285f95c39189251248
apg868 <123202954+apg868@users.noreply.github.com>
2023-08-13 00:41:44 -0400

    added fully functional Navbar

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,8 @@
+import React from "react";
+import { VStack, HStack, Image, Text, Link as ChakraLink, Icon} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import lemonLogo from "./assets_folder/LLlogo1.jpg"
+import { FaInstagram, FaFacebook, FaTwitter, FaTiktok } from "react-icons/fa";
 
 
 
@@ -6,8 +11,53 @@ function Footer(){
 
 
     return(
+        <HStack height="300px" width="100vw" bgColor="rgba(189, 195, 199, 0.3)"  borderColor="rgba(189,195,199)" spacing={40} justifyContent="center">
+            <Image src={lemonLogo} boxSize="175px" opacity="60%" borderRadius={10}/>
+            <VStack spacing={2}>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Doormat Navigation</Text>
+            <Text fontFamily="fontSecondary" fontWeight="medium" fontSize="md" color="gray">
+                <Link to="/">Home</Link>
+            </Text>
+            <Text fontFamily="fontSecondary" fontWeight="medium" fontSize="md" color="gray">
+                <Link to="/about">About</Link>
+            </Text>
+            <Text fontFamily="fontSecondary" fontWeight="medium" fontSize="md" color="gray">
+                <Link to="/reservations">Reservations</Link>
+            </Text>
+            <Text fontFamily="fontSecondary" fontWeight="medium" fontSize="md" color="gray">
+                <Link to="/menu">Menu</Link>
+            </Text>
+            </VStack>
+            <VStack>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Get in contact with us!</Text>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="medium" fontSize="xs"> Email: LittleLemonCo@LL.com </Text>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="medium" fontSize="xs"> Phone Number: (808) 123-4567 </Text>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="medium" fontSize="xs"> Address: 102 Maplewood Way  </Text>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="medium" fontSize="xs"> Address: 41 Commonbush Road  </Text>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="medium" fontSize="xs"> Address: 1404 Brickbush Blvd  </Text>
+            </VStack>
+            <VStack>
+            <Text color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Follow us on social media! </Text>
+            <HStack spacing={1}>
+            <ChakraLink href="https://instagram.com/" color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Instagram </ChakraLink>
+            <Icon as={FaInstagram} boxSize="15px"/>
+            </HStack>
+            <HStack spacing={1}>
+            <ChakraLink href="https://facebook.com/" color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Facebook </ChakraLink>
+            <Icon as={FaFacebook} boxSize="15px"/>
+            </HStack>
+            <HStack spacing={1}>
+            <ChakraLink href="https://twitter.com/" color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> Twitter </ChakraLink>
+            <Icon as={FaTwitter} boxSize="15px"/>
+            </HStack>
+            <HStack spacing={1}>
+            <ChakraLink href="https://tiktok.com/" color="gray" fontFamily="fontPrimary" fontWeight="bold" fontSize="md"> TikTok </ChakraLink>
+            <Icon as={FaTiktok} boxSize="15px"/>
+            </HStack>
+            </VStack>
 
-        <footer> hello <p> footer return paragraph</p> </footer>
+
+        </HStack>
 
     )
 }
--- src/HomePage/TestimonialsSection/Testimonials.js
+++ src/HomePage/TestimonialsSection/Testimonials.js
@@ -6,7 +6,6 @@ import stockTwo from "../../assets_folder/Test_Stocks/stock2.jpg"
 import stockThree from "../../assets_folder/Test_Stocks/stock3.jpg"
 import stockFour from "../../assets_folder/Test_Stocks/stock4.jpg"
 import stockFive from "../../assets_folder/Test_Stocks/stock5.jpg"
-import lemonIcon from "../../assets_folder/logoalt.png"
 
 
 function Testimonials(){
@@ -24,17 +23,13 @@ function Testimonials(){
     paddingBottom={300}
     spacing={100}
     >
-    <HStack spacing={8}>
-    <Image src={lemonIcon} width="140px" height="40px"/>
     <Text fontFamily="secondaryFont" fontSize="5xl" fontWeight="normal" color="#36454F"> See our authentic customer testimonials ! </Text>
-    <Image src={lemonIcon} width="140px" height="40px"/>
-    </HStack>
     <HStack spacing={12}>
         <TestimonialsFrame name="Boris" image={stockOne} rating="5" reviewText='"Wow what a delicious time! Only regret was not buying more food!"'/>
         <TestimonialsFrame name="Richard" image={stockTwo} rating="4.5" reviewText='"The lemon tart is just like my nonna used to make!'/>
         <TestimonialsFrame name="Olivia" image={stockThree} rating="5" reviewText='"Try the Bruschetta! It is some of the best in town!"'/>
         <TestimonialsFrame name="Emily" image={stockFour} rating="5" reviewText='I had a wonderful time. The service was impeccable.'/>
-        <TestimonialsFrame name="Don" image={stockFive} rating="5" reviewText='"Incredibly authentic, I really enjoyed this"'/>
+        <TestimonialsFrame name="Don" image={stockFive} rating="4" reviewText='"Don sipped delicious coffee 8 times out of 9!"'/>
     </HStack>
 
     </VStack>
